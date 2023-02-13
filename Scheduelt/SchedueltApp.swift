d66dbfb548011b713e6469a7291709625cde3f4f
Shahid Arshad <shahid.arshad@devsinc.com>
2023-02-13 16:57:35 +0500

    Initial Commit

---
+++ Scheduelt/SchedueltApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchedueltApp.swift
+//  Scheduelt
+//
+//  Created by Dev on 13/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SchedueltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
